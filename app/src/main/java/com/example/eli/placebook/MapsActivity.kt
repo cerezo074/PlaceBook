fa327c3d0a2c4e741e89636bcc571b9bc6ea59a5
Eli Pacheco Hoyos <eli@balancemy.money>
2019-05-10 07:35:16 -0500

    Chapter 14 User Location - Permissons and Current Location

--- app/src/main/java/com/example/eli/placebook/MapsActivity.kt
+++ app/src/main/java/com/example/eli/placebook/MapsActivity.kt
@@ -1,9 +1,13 @@
 package com.example.eli.placebook
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
+import android.support.v4.app.ActivityCompat
+import android.util.Log
+import com.google.android.gms.location.*
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -27,25 +31,48 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         setupLocationClient()
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+        getCurrentLocation()
+    }
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if (requestCode == REQUEST_LOCATION) {
+          if (grantResults.size == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+              getCurrentLocation()
+          } else {
+              Log.e(TAG, "Location permissons denied")
+          }
+        }
     }
 
     private fun setupLocationClient() {
         fusedLocationProvider = LocationServices.getFusedLocationProviderClient(this)
+    }
+
+    private fun requestLocationPermissions() {
+        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_LOCATION)
+    }
+
+    private fun getCurrentLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestLocationPermissions()
+        } else {
+            mMap.isMyLocationEnabled = true
+            fusedLocationProvider.lastLocation.addOnCompleteListener {
+                if (it.result != null) {
+                    val latLng = LatLng(it.result?.latitude ?: 0.0, it.result?.longitude ?: 0.0)
+                    val update = CameraUpdateFactory.newLatLngZoom(latLng, 16.0f)
+                    mMap.moveCamera(update)
+                } else {
+                    Log.e(TAG, "No location found")
+                }
+            }
+        }
+    }
+
+    companion object {
+        private const val REQUEST_LOCATION = 1
+        private const val TAG = "MapsActivity"
     }
 }
